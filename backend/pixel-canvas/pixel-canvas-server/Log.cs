81cb433327b97f88ce3afd0f6020b38947291ee1
BTELNYY <btelnyy@gmail.com>
2023-07-02 20:25:53 -0600

    beginnings of a back end

---
+++ backend/pixel-canvas/pixel-canvas-server/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pixel_canvas_server
+{
+    public static class Log
+    {
+        public static void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Error(string message)
+        {
+            Utility.WriteLineColor("Error: " + message, ConsoleColor.Red);
+        }
+
+        public static void Warning(string message)
+        {
+            Utility.WriteLineColor("Warning: " + message, ConsoleColor.Yellow);
+        }
+
+        public static void Debug(string message)
+        {
+            Utility.WriteLineColor("Debug: " + message, ConsoleColor.Gray);
+        }
+
+        public static void Success(string message)
+        {
+            Utility.WriteLineColor("Success: " + message, ConsoleColor.Green);
+        }
+    }
+
+    public class Utility
+    {
+        public static void WriteLineColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ backend/pixel-canvas/pixel-canvas-server/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pixel_canvas_server
+{
+    public static class Server
+    {
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        //static Thread ConsoleThread = new(ConsoleHandler.HandleCommands);
+        public static void Start()
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Parse(Program.BindIP), Program.Port);
+            TcpClient clientSocket;
+            int counter = 0;
+            serverSocket.Start();
+            Log.Info("Accepting client connections.");
+            //ConsoleThread.Start();
+            counter = 0;
+            Log.Info("Server ready.");
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Log.Debug("Client connected. ID: " + counter.ToString());
+                Client client = new Client();
+                client.ClientID = counter;
+                var source = new CancellationTokenSource();
+                client.StartClient(clientSocket, counter, source.Token, source);
+                clients.Add(counter, client);
+            }
+        }
+    }
+}
