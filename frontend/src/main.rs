078940e9f935730b0acc54b5ea99089fdbb9cf02
webmsgr <zapcontact000@gmail.com>
2023-07-02 18:41:24 -0700

    update things

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -39,41 +39,62 @@ fn main() {
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
+    canvas.set_width(WIDTH as u32);
+    canvas.set_height(HEIGHT as u32);
     let context = canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
+    let b = Box::new([0u8; WIDTH*HEIGHT*3]);
+    let render = Box::new([0u8; WIDTH*HEIGHT*3]);
     // lets create a test image
-    let init = vec![0u8; width*height*4];
+    //let init = vec![0u8; WIDTH*HEIGHT*3];
     
     let game = Game {
-        pixels: init,
+        pixels: b,
         context,
+        render,
         tick: 0
     };
-
-    game_loop(game, 60, 0.5, |mut g| {
-        g.game.tick += 1;
-        for (offset, pixel) in g.game.pixels.chunks_exact_mut(4).enumerate() {
-            let id = ((offset + g.game.tick) % u32::MAX as usize) as u32;
-            let px = id.to_le_bytes();
-            pixel.copy_from_slice(&[px[0], px[1], px[2], 255]);
-
-        }       
-    }, |g| {
-        let image_data = ImageData::new_with_u8_clamped_array(Clamped(g.game.pixels.as_slice()), width as u32).unwrap();
+    let mut frame_buffer = Box::new([0u8; WIDTH*HEIGHT*4]);
+    game_loop(game, 60, 0.5, |g| {
+        g.game.tick();
+    }, move |g| {
+        g.game.render();
+        for (offset, pixel) in g.game.render.chunks_exact(3).enumerate() {
+            let px = [pixel[0], pixel[1], pixel[2], 255];
+            frame_buffer[offset*4..offset*4+4].copy_from_slice(&px);
+        }
+        let image_data = ImageData::new_with_u8_clamped_array(Clamped(frame_buffer.as_slice()), WIDTH as u32).unwrap();
         g.game.context.put_image_data(&image_data, 0.0, 0.0).unwrap();
     });
 }
 
 
-const width: usize = 1024;
-const height: usize = 1024;
+const WIDTH: usize = 1000;
+const HEIGHT: usize = 1000;
+
+
+impl Game {
+    fn tick(&mut self) {
+        self.tick += 1;
+        for (offset, pixel) in self.pixels.chunks_exact_mut(3).enumerate() {
+            let id = ((offset + self.tick) % u32::MAX as usize) as u32;
+            let px = id.to_le_bytes();
+            pixel.copy_from_slice(&[px[0], px[1], px[2]]);
+
+        }       
+    }
+    fn render(&mut self) {
+        self.render.copy_from_slice(&self.pixels[..]);
+    }
+}
 
 struct Game {
-    pixels: Vec<u8>,
+    pixels: Box<[u8; WIDTH*HEIGHT*3]>,
+    render: Box<[u8; WIDTH*HEIGHT*3]>,
     context: CanvasRenderingContext2d,
     tick: usize
 }
